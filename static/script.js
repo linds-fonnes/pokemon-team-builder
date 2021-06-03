294a5010c3f91313619fe06c1c6a0d96e2eff256
Lindsey <linds.fonnes@gmail.com>
2021-06-02 20:56:43 -0600

    pokemon data for card displays-add route to add pokemon to team next

--- static/script.js
+++ static/script.js
@@ -34,20 +34,33 @@ async function getPokemonData(term) {
             "src",
             response.data.sprites.other.dream_world.front_default
           );
-        document.getElementById("pokemon-name").textContent =
-          response.data.name.toUpperCase();
+        document.getElementById("pokemon-card").classList.remove("hidden");
+        const pokemon_name = document.getElementById("pokemon-name");
+        pokemon_name.textContent = response.data.name.toUpperCase();
+        const types = response.data.types;
+        for (let i = 0; i < types.length; i++) {
+          const type_el = document.createElement("span");
+          type_el.textContent = ` ${response.data.types[i].type.name} `;
+          pokemon_name.appendChild(type_el);
+        }
+
         const stat_name = document.getElementsByClassName("pokemon-stat-name");
-        const base_stat = document.getElementsByClassName("stat");
+        const base_stat = document.getElementsByClassName("pokemon-stat");
 
         for (let i = 0; i < stat_name.length; i++) {
           stat_name[i].textContent = response.data.stats[i].stat.name;
           base_stat[i].textContent = response.data.stats[i].base_stat;
         }
+        const card = document.getElementById("pokemon-card");
+        const btn = document.createElement("button");
+        btn.textContent = "Add to Team";
+        card.appendChild(btn);
         hideLoader();
       });
   } catch (error) {
     hideLoader();
     search_error.classList.remove("hidden");
+    document.getElementById("pokemon-card").classList.add("hidden");
   }
 }
 
