ae54439209b1a21bec25ac84843066dbc3f3c4b0
Lindsey <linds.fonnes@gmail.com>
2021-06-20 20:00:23 -0600

    functionality for calculating dual type pokemon and displaying their stats in table

--- static/script.js
+++ static/script.js
@@ -92,51 +92,113 @@ async function processPokemonSearch(evt) {
   }
 }
 
+function displayDualTypeDamageRelations(type){
+  console.log(type)
+  for(immunity of type.immune_to){
+    console.log("IMMUNE TO", immunity)
+    let curr_value = parseInt($(`.${immunity}#immune_to`).text())
+    $(`.${immunity}#immune_to`).html(`${curr_value + 1}`)
+  }
+  for(resist of type.resists){
+    console.log("RESISTS", resist)
+    let curr_value = parseInt($(`.${resist}#resists`).text())
+    $(`.${resist}#resists`).html(`${curr_value + 1}`)
+  }
+  for(weakness of type.weak_against){
+    console.log("WEAK AGAINST", weakness)
+    let curr_value = parseInt($(`.${weakness}#weak_against`).text())
+    $(`.${weakness}#weak_against`).html(`${curr_value + 1}`)
+  }
+  // for(let i = 0; i < data.damage_relations.length; i++){
+  //   console.log(data.damage_relations[i])
+  //   console.log(data.damage_relations[i].type)
+  //   for(resist of data.damage_relations[i].resists){
+  //     console.log("RESISTS",resist)
+  //     let curr_value = parseInt($(`.${resist}#resists`).text())
+  //     $(`.${resist}#resists`).html(`${curr_value + 1}`)
+      
+  //   }
+  //   for(immunity of data.damage_relations[i].immune_to){
+  //     console.log("IMMUNE TO",immunity)
+  //     let curr_value = parseInt($(`.${immunity}#immune_to`).text())
+  //     $(`.${immunity}#immune_to`).html(`${curr_value + 1}`)
+  //   }
+  //   for(weakness of data.damage_relations[i].weak_against){
+  //     console.log("WEAK AGAINST", weakness)
+  //     let curr_value = parseInt($(`.${weakness}#weak_against`).text())
+  //     $(`.${weakness}#weak_against`).html(`${curr_value + 1}`)
+  //   }
+  // }
+}
 
-function displayDualTypeDamageRelations(data){
+function calculateDualTypeDamageRelations(data){
   console.log("THERES TWO TYPES", data)
-  let weak_arr = []
+  let temp_weak_arr = []
+  let temp_resist_arr = []
   let immune_arr = []
   for(type of data){
-    console.log(type)
     //for each type of current pokemon, push weakness into a single arr
     for(let i = 0; i < type.weak_against.length; i++){
-      weak_arr.push(type.weak_against[i])
+      temp_weak_arr.push(type.weak_against[i])
     }
     //for each type of current pokemon, push immunities into a single arr
     for(let i =0; i < type.immune_to.length; i++){
       immune_arr.push(type.immune_to[i])
     }
     //filter through the weaknesses and immunities and return only the type names that aren't in the immunity arr
-    let filtered_arr = weak_arr.filter((word) => !immune_arr.includes(word))
-    console.log("HERE",immune_arr,weak_arr,filtered_arr) 
+    let new_weak_arr = temp_weak_arr.filter((word) => !immune_arr.includes(word))
+    
+    //for each type of current pokemon, push the resistances into a single arr
+    for(let i = 0; i < type.resists.length; i++){
+      temp_resist_arr.push(type.resists[i])
+    }
+    // filter through the resistances and immunities and returns only the type names that aren't in the immunity arr
+    let new_resist_arr = temp_resist_arr.filter((word) => !immune_arr.includes(word))
+
+    // filter through new resist arr and new weakness array and remove any overlaps from the new resist array
+    let newest_resist_arr = new_resist_arr.filter((word) => !new_weak_arr.includes(word))
+    let newest_weakness_arr = new_weak_arr.filter((word) => !new_resist_arr.includes(word))
+    //making sure there are no duplicates in any of arrays
+    let immune = [...new Set(immune_arr)]
+    let weakness = [...new Set(newest_weakness_arr)]
+    let resist = [...new Set(newest_resist_arr)]
+    //resetting the value of the type's damage relations to be all of the filtered arrays
+    type.weak_against = weakness
+    type.resists = resist
+    type.immune_to = immune
   }
+  displayDualTypeDamageRelations(type)
 }
 
 function displayDamageRelations(resp){
   $(".stat").html("0")
+  console.log("ORIGINAL RESPONSE",resp)
+  console.log("RESP.DATA", resp.data)
   for(data of resp.data){
+    console.log("THIS IS THE DATA", data)
     if(data.damage_relations.length > 1) {
-      return displayDualTypeDamageRelations(data.damage_relations)
+     calculateDualTypeDamageRelations(data.damage_relations)
     }
-    for(let i = 0; i < data.damage_relations.length; i++){
-      console.log(data.damage_relations[i])
-      console.log(data.damage_relations[i].type)
-      for(resist of data.damage_relations[i].resists){
-        console.log("RESISTS",resist)
-        let curr_value = parseInt($(`.${resist}#resists`).text())
-        $(`.${resist}#resists`).html(`${curr_value + 1}`)
-        
-      }
-      for(immunity of data.damage_relations[i].immune_to){
-        console.log("IMMUNE TO",immunity)
-        let curr_value = parseInt($(`.${resist}#immune_to`).text())
-        $(`.${immunity}#immune_to`).html(`${curr_value + 1}`)
-      }
-      for(weakness of data.damage_relations[i].weak_against){
-        console.log("WEAK AGAINST", weakness)
-        let curr_value = parseInt($(`.${weakness}#weak_against`).text())
-        $(`.${weakness}#weak_against`).html(`${curr_value + 1}`)
+    else{
+      for(let i = 0; i < data.damage_relations.length; i++){
+        console.log(data.damage_relations[i])
+        console.log(data.damage_relations[i].type)
+        for(resist of data.damage_relations[i].resists){
+          console.log("RESISTS",resist)
+          let curr_value = parseInt($(`.${resist}#resists`).text())
+          $(`.${resist}#resists`).html(`${curr_value + 1}`)
+          
+        }
+        for(immunity of data.damage_relations[i].immune_to){
+          console.log("IMMUNE TO",immunity)
+          let curr_value = parseInt($(`.${immunity}#immune_to`).text())
+          $(`.${immunity}#immune_to`).html(`${curr_value + 1}`)
+        }
+        for(weakness of data.damage_relations[i].weak_against){
+          console.log("WEAK AGAINST", weakness)
+          let curr_value = parseInt($(`.${weakness}#weak_against`).text())
+          $(`.${weakness}#weak_against`).html(`${curr_value + 1}`)
+        }
       }
     }
   }
